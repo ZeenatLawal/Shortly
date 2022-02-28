cd841f660c36bedbd0a6e7310cd2ff6ea665fe20
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-02-28 15:30:45 +0100

    Remove unused import statement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
