29b8e23d9d5e39f9e7efff48853a1f0578d112cb
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-02-28 15:23:06 +0100

    Create form component for inputting URL

---
+++ src/components/InputUrl.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { shortenedUrl } from '../redux/shortUrlData/shortUrl';
+
+const InputUrl = () => {
+  const dispatch = useDispatch();
+  const [url, setUrl] = useState('');
+
+  const onUrlChange = (e) => setUrl(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(shortenedUrl(url));
+    setUrl('');
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={onUrlChange} required />
+      <button type="submit">Shorten it!</button>
+    </form>
+  );
+};
+
+export default InputUrl;
