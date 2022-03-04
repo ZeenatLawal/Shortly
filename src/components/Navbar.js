512d0c7b7871254dcf0908d9ee4e4e1f41051cc3
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-03-04 09:40:34 +0100

    Create Navbar component and stylesheet

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import '../styles/Navbar.css';
+
+const Navpanel = () => (
+  <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Container>
+      <Navbar.Brand>Shortly</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="align-items-center">
+          <Nav.Link>Features</Nav.Link>
+          <Nav.Link>Pricing</Nav.Link>
+          <Nav.Link>Resources</Nav.Link>
+        </Nav>
+        <Nav className="d-lg-none">
+          <hr />
+        </Nav>
+        <Nav className="align-items-center">
+          <Nav.Link>Login</Nav.Link>
+          <Nav.Link>Sign Up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default Navpanel;
