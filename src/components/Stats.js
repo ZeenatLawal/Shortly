0147ed9ed57edfa3bb3b531c51e1692374d17d4f
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-03-07 11:00:27 +0100

    Add Boost section

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,39 +4,45 @@ import records from '../images/icon-detailed-records.svg';
 import customize from '../images/icon-fully-customizable.svg';
 
 const Stats = () => (
-  <section>
-    <h3>Advanced Statistics</h3>
-    <p>
-      Track how your links are performing across the
-      web with our advanced statistics dashboard.
-    </p>
-    <div>
+  <>
+    <section>
+      <h3>Advanced Statistics</h3>
+      <p>
+        Track how your links are performing across the
+        web with our advanced statistics dashboard.
+      </p>
       <div>
-        <img src={brand} alt="Graph icon" />
-        <h4>Brand recognition</h4>
-        <p>
-          Boost your brand recognition with each click. Generic links don&apos;t mean a thing.
-          Branded links help instill confidence in your content.
-        </p>
+        <div>
+          <img src={brand} alt="Graph icon" />
+          <h4>Brand recognition</h4>
+          <p>
+            Boost your brand recognition with each click. Generic links don&apos;t mean a thing.
+            Branded links help instill confidence in your content.
+          </p>
+        </div>
+        <div>
+          <img src={records} alt="Records icon" />
+          <h4>Detailed Records</h4>
+          <p>
+            Gain insights into who is clicking your links. Knowing when and where people engage with
+            your content helps inform better decisions.
+          </p>
+        </div>
+        <div>
+          <img src={customize} alt="Pens icon" />
+          <h4>Fully Customizable</h4>
+          <p>
+            Improve brand awareness and content discoverability through customizable links,
+            supercharging audience engagement.
+          </p>
+        </div>
       </div>
-      <div>
-        <img src={records} alt="Records icon" />
-        <h4>Detailed Records</h4>
-        <p>
-          Gain insights into who is clicking your links. Knowing when and where people engage with
-          your content helps inform better decisions.
-        </p>
-      </div>
-      <div>
-        <img src={customize} alt="Pens icon" />
-        <h4>Fully Customizable</h4>
-        <p>
-          Improve brand awareness and content discoverability through customizable links,
-          supercharging audience engagement.
-        </p>
-      </div>
-    </div>
-  </section>
+    </section>
+    <section>
+      <h3>Boost your links today</h3>
+      <button type="button">Get Started</button>
+    </section>
+  </>
 );
 
 export default Stats;
