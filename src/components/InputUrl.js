df45f5f03da577b8e3a749dbb550407b20d28e10
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-03-16 19:42:10 +0100

    Style InputUrl section for mobile

--- src/components/InputUrl.js
+++ src/components/InputUrl.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { shortenedUrl } from '../redux/shortUrlData/shortUrl';
+import '../styles/InputUrl.css';
 
 const InputUrl = () => {
   const dispatch = useDispatch();
@@ -19,22 +20,29 @@ const InputUrl = () => {
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="url"
-          placeholder="Shorten a link here..."
-          data-testid="url-input"
-          onChange={onUrlChange}
-          onInvalid={onInvalidUrl}
-          onInput={onInputUrl}
-          required
-        />
-        <button type="submit" data-testid="button">Shorten it!</button>
-      </form>
-      {validity && (
-        <p data-testid="error-msg">{validity}</p>
-      )}
+    <section className="mid-section">
+      <div className="input-section rounded main">
+        <form onSubmit={handleSubmit} className="row p-0">
+          <div className="col-12 col-md-9 pe-md-0">
+            <input
+              type="url"
+              placeholder="Shorten a link here..."
+              data-testid="url-input"
+              onChange={onUrlChange}
+              onInvalid={onInvalidUrl}
+              onInput={onInputUrl}
+              className="mt-2 mb-3 my-md-0 form-control"
+              required
+            />
+          </div>
+          <div className="col-12 col-md-3 ps-md-0">
+            <button type="submit" data-testid="button" className="shorten form-control text-center">Shorten it!</button>
+          </div>
+        </form>
+        {validity && (
+          <p data-testid="error-msg">{validity}</p>
+        )}
+      </div>
     </section>
   );
 };
