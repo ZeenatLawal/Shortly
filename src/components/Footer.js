1329fda2b03957185345100b23418db7c4a19129
Zeenat Lawal <zeenatlawal82@gmail.com>
2022-03-29 13:57:11 +0100

    Style Footer section for desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,32 +8,34 @@ import '../styles/Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <div className="main d-flex flex-column align-items-center">
+    <div className="main d-flex flex-column align-items-center flex-lg-row align-items-lg-start">
       <img src={logo} alt="Shortly Logo" className="footer-logo" />
-      <div>
-        <h6>Features</h6>
-        <p>Link Shortening</p>
-        <p>Branded Links</p>
-        <p>Analytics</p>
-      </div>
-      <div>
-        <h6>Resources</h6>
-        <p>Blog</p>
-        <p>Developers</p>
-        <p>Support</p>
-      </div>
-      <div>
-        <h6>Company</h6>
-        <p>About</p>
-        <p>Our Team</p>
-        <p>Careers</p>
-        <p>Contact</p>
-      </div>
-      <div className="pt-4 pb-5">
-        <img src={facebook} alt="Facebook icon" className="pe-4 footer-icon" />
-        <img src={twitter} alt="Twitter icon" className="pe-4 footer-icon" />
-        <img src={pinterest} alt="Pinterest icon" className="pe-4 footer-icon" />
-        <img src={instagram} alt="Instagram icon" className="footer-icon" />
+      <div className="d-lg-flex flex-lg-row justify-content-lg-between w-100">
+        <div>
+          <h6>Features</h6>
+          <p>Link Shortening</p>
+          <p>Branded Links</p>
+          <p>Analytics</p>
+        </div>
+        <div>
+          <h6>Resources</h6>
+          <p>Blog</p>
+          <p>Developers</p>
+          <p>Support</p>
+        </div>
+        <div>
+          <h6>Company</h6>
+          <p>About</p>
+          <p>Our Team</p>
+          <p>Careers</p>
+          <p>Contact</p>
+        </div>
+        <div className="pt-4 pt-lg-0">
+          <img src={facebook} alt="Facebook icon" className="pe-4 footer-icon" />
+          <img src={twitter} alt="Twitter icon" className="pe-4 footer-icon" />
+          <img src={pinterest} alt="Pinterest icon" className="pe-4 footer-icon" />
+          <img src={instagram} alt="Instagram icon" className="footer-icon" />
+        </div>
       </div>
     </div>
   </footer>
